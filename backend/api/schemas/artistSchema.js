8ac640aac2596177b4d7a3601ac59a78029088d8
loulevin <patricia.balbier@gmail.com>
2024-03-11 17:03:08 +0100

    originalPublisherSchema added and update artistSchema

--- backend/api/schemas/artistSchema.js
+++ backend/api/schemas/artistSchema.js
@@ -5,6 +5,14 @@ import { personInfoSubdocument } from './personInfoSubdocument.js'
 export const artistSchema = new mongoose.Schema(
     {
         ...personInfoSubdocument.obj,
+        edition: [
+            {
+                edition_id: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'Edition',
+                },
+            },
+        ],
     },
     {
         collection: 'artist',
