38497a10139c6973d7cb7a88122404fe24ed996b
Selman <selman.ciftci.coding@gmail.com>
2024-03-11 11:03:15 +0100

    Selman: server is working

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+
+const PORT = 3550
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const dbURL = process.env.DB_URL
+
+const connectToDb = async () => {
+    try {
+        await mongoose.connect(dbURL)
+        console.log(`Conntected to mongoDB`)
+    } catch (e) {
+        console.error(e)
+        console.log(`Connection to mongoDB failed`)
+    }
+}
+
+const startServer = async () => {
+    try {
+        await connectToDb()
+        app.listen(PORT, () => {
+            console.log(`Conntected to server on: ${PORT}`)
+        })
+    } catch (e) {
+        console.log(`Connection to server on port ${PORT} failed`)
+        throw new Error(e.message)
+    }
+}
+
+startServer()
