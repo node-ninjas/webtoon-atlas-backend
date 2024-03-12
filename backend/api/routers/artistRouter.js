03ffa754236aaefc92aa3bc34dbce9a10a121515
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-12 11:13:38 +0100

    added artistRouter

---
+++ backend/api/routers/artistRouter.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import {
+    addSingleArtist,
+    getSingleArtist,
+    getAllArtists,
+    updateSingleArtist,
+    deleteSingleArtist,
+    deleteAllArtists,
+} from '../controllers/artistController.js'
+
+const artistRouter = express.Router()
+
+artistRouter
+    .route('/')
+    .get(getAllArtists)
+    .post(addSingleArtist)
+    .delete(deleteAllArtists)
+artistRouter
+    .route('/:id')
+    .get(getSingleArtist)
+    .patch(updateSingleArtist)
+    .delete(deleteSingleArtist)
