5d6ead251ba4d06bd1e73adf423c5f577e5a3030
Selman <selman.ciftci.coding@gmail.com>
2024-03-13 10:53:53 +0100

    Selman: Update schemas
    * added types to statusSchema
    
    * Refactor editionSchema and statusSchema

--- backend/api/schemas/subDocuments/statusSchema.js
+++ backend/api/schemas/subDocuments/statusSchema.js
@@ -2,7 +2,16 @@ import mongoose from 'mongoose'
 
 export const statusSchema = new mongoose.Schema({
     status: {
-        type: [String],
+        type: String,
         required: true,
+    },
+    originalStatus: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ['complete', 'running', 'onHiatus', 'aborted', 'announced'],
     },
 })
