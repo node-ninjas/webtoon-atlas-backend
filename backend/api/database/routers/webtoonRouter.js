3d431e6b55fb9d73a1ab2ff524c98ad9a675df93
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-14 12:48:57 +0100

    webtoonRouter in

---
+++ backend/api/database/routers/webtoonRouter.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import {
+    addSingleWebtoon,
+    getSingleWebtoon,
+    getAllWebtoons,
+    updateSingleWebtoon,
+    deleteSingleWebtoon,
+    deleteAllWebtoons,
+} from '../controllers/webtoonController.js'
+
+export const webtoonRouter = express.Router()
+
+webtoonRouter
+    .route('/')
+    .get(getAllWebtoons)
+    .post(addSingleWebtoon)
+    .delete(deleteAllWebtoons)
+webtoonRouter
+    .route('/:id')
+    .get(getSingleWebtoon)
+    .patch(updateSingleWebtoon)
+    .delete(deleteSingleWebtoon)
