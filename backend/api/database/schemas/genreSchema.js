c84ce737126642853fb32957ba3bc71ae5cec0b5
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-17 01:09:08 +0100

    Refactor genreSchema and update genre in genre.rest

--- backend/api/database/schemas/genreSchema.js
+++ backend/api/database/schemas/genreSchema.js
@@ -1,19 +1,10 @@
 import mongoose from 'mongoose'
-import { genres } from './subDocuments/genres.js'
 
 export const genreSchema = new mongoose.Schema(
     {
-        type: {
+        genre: {
             type: String,
             required: true,
-            validate: {
-                validator: function (v) {
-                    return genres.includes(v)
-                },
-                message: (props) => {
-                    return `${props.value} is not a valid genre. REASON: ${props.reason}`
-                },
-            },
         },
     },
     {
