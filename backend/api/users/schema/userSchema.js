750fbe2935c6d7a1c46de43db90f4d3e711f851b
loulevin <patricia.balbier@gmail.com>
2024-03-18 13:46:53 +0100

    add user schema file

---
+++ backend/api/users/schema/userSchema.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+export const userSchema = new mongoose.Schema(
+    {
+        userName: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        type: {
+            type: [String],
+            required: true,
+            enum: ['user', 'admin', 'webtoonDatabase'],
+        },
+        role: { type: String, required: true },
+        age: { type: Number, required: true },
+    },
+    {
+        collection: 'users',
+        timestamps: true,
+    }
+)
+
+export const User = mongoose.model('User', userSchema)
