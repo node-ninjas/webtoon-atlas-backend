9f95a8b3946c455dfd5a345d1475788a5a70b286
loulevin <patricia.balbier@gmail.com>
2024-03-18 13:50:10 +0100

    rename password to hash

--- backend/api/users/schema/userSchema.js
+++ backend/api/users/schema/userSchema.js
@@ -4,7 +4,7 @@ export const userSchema = new mongoose.Schema(
     {
         userName: { type: String, required: true, unique: true },
         email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
+        hash: { type: String, required: true },
         type: {
             type: [String],
             required: true,
